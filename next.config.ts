5459c6c58aaa222b28820991f55bc31967600041
xcodeflow-cmd <xcodeflow@gmail.com>
2026-04-18 18:20:48 +0300

    da

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,21 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.virgilagu.ro"
+          }
+        ],
+        destination: "https://virgilagu.ro/:path*",
+        permanent: true
+      }
+    ];
+  },
   images: {
     remotePatterns: [
       {
